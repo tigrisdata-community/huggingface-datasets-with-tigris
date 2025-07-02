f70f12ac22d9a03cc344aea6d77987bc098777f8
Xe Iaso <xe@tigrisdata.com>
2025-07-02 16:58:08 +0000

    fix(import-to-tigris): remove assert that dataset isn't already in the bucket
    Signed-off-by: Xe Iaso <xe@tigrisdata.com>

--- scripts/import-to-tigris.py
+++ scripts/import-to-tigris.py
@@ -14,8 +14,6 @@ def main():
     dataset_name = os.getenv("DATASET_NAME")
     assert dataset_name is not None
 
-    assert not fs.exists(f"/{bucket_name}/datasets/{dataset_name}"), f"{dataset_name} is already present in {bucket_name}"
-
     dataset = load_dataset(dataset_name, split="train")
     dataset.save_to_disk(f"s3://{bucket_name}/datasets/{dataset_name}", storage_options=storage_options)
 
